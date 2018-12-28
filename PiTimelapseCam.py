bd3c65bbe1c2c302d670e29fbe89bb55e6573bac
Chris Rowley <code@chrisrowley.net>
2018-12-28 17:23:51 +1100

    Added Directory to Image Capture Path

--- PiTimelapseCam.py
+++ PiTimelapseCam.py
@@ -23,7 +23,8 @@ create_photo_dir(newdir)
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+
+for filename in camera.capture_continuous('newdir/img{counter:03d}.jpg'):
     print('Captured %s' % filename)
     sleep(60) # wait 1 minutes
 
